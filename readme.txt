0ce133043ba1e06df5ac1873ee21930ed5feb5e7
Matt Chan <matt@mattchan.net>
2012-03-12 12:06:37 -0400

    updated readme, added screenshot, renamed plugin file

--- readme.txt
+++ readme.txt
@@ -1,22 +1,21 @@
 === Plugin Name ===
 Contributors: mattchan
-Tags: comments, posts
-Requires at least: 3.0.2
+Tags: posts, runkeeper, button
+Requires at least: 3.2
 Tested up to: 3.3.1
 Stable tag: trunk
 
-Add a simple RunKeeper button to your posts.
+Add a simple RunKeeper button to the top of your posts.
 
 == Description ==
 
-Adds a RunKeeper Healthy button to prepended to the post content. Will open a new
-window to 
+Adds a RunKeeper Healthy button to prepended to the post content.
 
 == Installation ==
 
-1. Upload the plugin folder to the `/wp-content/plugins/` directory
-2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Configure as necessary.
+1. Upload the files to the `/wp-content/plugins/wp_runkeeper_button` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Done.
 
 == Frequently Asked Questions ==
 
@@ -24,7 +23,11 @@ window to
 
 Please post issues on the GitHub repo.
 
-http://github.com/mattchan/wp_runkeeper_btn
+https://github.com/mattchan/wp_runkeeper_button/issues
+
+== Screenshots ==
+
+1. RunKeeper button on a post.
 
 == Changelog ==
 
--- wp_runkeeper_button.php
+++ wp_runkeeper_button.php
@@ -5,16 +5,15 @@ Plugin URI: https://github.com/mattchan/wp_runkeeper_button
 Version: 0.1
 Author: Matt Chan
 Author URI: http://mattchan.net/
-License: GPL2
+License: GPLv2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
-?>
 
-<?php
-/*  Copyright 2012 Matthew Chan (email : matt@mattchan.net)
+/*  Copyright 2012 Matthew Chan  (email : matt@mattchan.net)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,10 +22,12 @@ License: GPL2
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-?>
 
+
+
+?>
 
 <?php
 
@@ -36,8 +37,8 @@ function add_runkeeper_btn($content) {
     if (is_single()) {
         return '<a href="http://runkeeper.com/share?healthyURL=' . get_permalink() . '"><img src="http://runkeeper.com/static/kronos/images/HealthyButton.png" class="healthyImage" alt="RunKeeper Healthy Button"></a><br /><br />' . $content;
     } else {
-	    return $content;
-	}
+        return $content;
+    }
 }
 
 
