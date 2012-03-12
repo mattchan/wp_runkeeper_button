7ba2e861987eb5bd077f03cc59479ef2f91e70a9
Matt Chan <matt@mattchan.net>
2012-03-12 12:09:06 -0400

    fixed license text

--- wp_runkeeper_button.php
+++ wp_runkeeper_button.php
@@ -10,10 +10,10 @@ License: GPLv2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 /*  Copyright 2012 Matthew Chan  (email : matt@mattchan.net)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,7 +22,7 @@ License: GPLv2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
